0c7b29dcc5aac59ed5cc3313a9a41ebbd04e351c
Lyonel Vincent <lyonel@ezix.org>
2013-12-27 20:48:37 +0100

    use a symbolic icon instead of a unicode character

--- extension.js
+++ extension.js
@@ -16,8 +16,7 @@ const WindowList = new Lang.Class({
 	_init: function(){
 		this.parent(0.0, 'All Windows');
 
-        	this.statusLabel = new St.Label({ text: '\u2630' });
-        	this.actor.add_actor(this.statusLabel);
+		this.actor.add_child(new St.Icon({ icon_name: 'view-grid-symbolic', style_class: 'system-status-icon' }));
         	this.updateMenu();
 	},
 
